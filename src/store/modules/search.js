65ace13db93087c1c5b8d06950d6b78fd1fbf069
PewPewService <darkinsog45@gmail.com>
2021-05-25 17:52:52 +0400

    added: types in store, sass in styles

--- src/store/modules/search.js
+++ src/store/modules/search.js
@@ -15,27 +15,22 @@ const getters = {
 const actions={
     async Search({ commit }, repository) {
         try{const response = await axios.get("https://api.github.com/repos/"+repository+"/forks");
-        //commit('searchData',response.data);
         commit({
             type: "searchData",
             data: response.data,
             exists: true
         });
-        //commit('existing', true);
         } catch (err){
-            //commit('searchData','');
             commit({
                 type: "searchData",
                 data: "",
                 exists: false
             })
-            //commit('existing', false);
         }
     }
 }
 
 const mutations = {
-    //searchData: (state, forks) => (state.forks=forks),
     searchData (state, forks){
         state.forks = forks.data;
         state.exists = forks.exists;
